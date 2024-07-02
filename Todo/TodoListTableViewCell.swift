18ce5c3d23a144f923110a5a69968d8ced00db08
홍정민 <dream7739@naver.com>
2024-07-02 22:30:13 +0900

    todoListVC 테이블 뷰 구성 todoListVC realm 데이터 조회 기능 구현

--- Todo/TodoListTableViewCell.swift
+++ Todo/TodoListTableViewCell.swift
@@ -5,4 +5,39 @@
 //  Created by 홍정민 on 7/2/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class TodoListTableViewCell: BaseTableViewCell {
+    let titleLabel = UILabel()
+    let contentLabel = UILabel()
+    let deadlineLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentLabel)
+        contentView.addSubview(deadlineLabel)
+    }
+    
+    override func configureLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(10)
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(10)
+        }
+        
+        deadlineLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentLabel.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(10)
+        }
+    }
+    
+    override func configureUI() {
+        titleLabel.font = .boldSystemFont(ofSize: 17)
+        contentLabel.font = .systemFont(ofSize: 14)
+        deadlineLabel.font = .systemFont(ofSize: 13)
+    }
+}
--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
+import SnapKit
+import RealmSwift
 
 class TodoListViewController: BaseViewController {
+    
+    let tableView = UITableView()
+
+    var list: Results<Todo>!
+    
+    let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        list =  realm.objects(Todo.self)
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
     
     override func configureUI() {
         let add = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonClicked))
         navigationItem.leftBarButtonItem = add
+        navigationItem.title = "전체"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureTableView(){
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TodoListTableViewCell.self, forCellReuseIdentifier: TodoListTableViewCell.identifier)
     }
     
     @objc
@@ -20,4 +58,27 @@ class TodoListViewController: BaseViewController {
         addTodoVC.modalPresentationStyle = .fullScreen
         present(addTodoVC, animated: true)
     }
+}
+
+extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TodoListTableViewCell.identifier) as! TodoListTableViewCell
+        let data = list[indexPath.row]
+        cell.titleLabel.text = data.title
+        cell.contentLabel.text = data.content
+        
+        let dataFormatter = DateFormatter()
+        dataFormatter.dateFormat = "yyyy-MM-dd"
+        cell.deadlineLabel.text = dataFormatter.string(for: data.deadLine)
+        return cell
+    }
+    
+ 
+    
 }
